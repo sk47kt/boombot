9209e4227ed5fad994557ed2921829a2dd210bb1
sk47kt <65600966+sk47kt@users.noreply.github.com>
2020-12-25 18:50:45 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,17 +3,16 @@ import pyupbit
 import time
 import os
 client = discord.Client()
-xrpprice = pyupbit.get_current_price("KRW-XRP")
-df = pyupbit.get_ohlcv("KRW-XRP", count=3)
-sung=str(round(xrpprice/304,3))
-baek=str(round(xrpprice/350,3))
 @client.event
 async def on_ready():
     print(f'{client.user} 에 로그인하였습니다!')
-
 @client.event
 async def on_message(message):
     if message.content.startswith('리플'):
+        xrpprice = pyupbit.get_current_price("KRW-XRP")
+        df = pyupbit.get_ohlcv("KRW-XRP", count=3)
+        sung=str(round(xrpprice/304,3))
+        baek=str(round(xrpprice/350,3))
         await message.channel.send('현재가 : {0}'.format(xrpprice))
         await message.channel.send('최근3일가격{0}'.format(df))
         await message.channel.send('백종인[350] 수익률{0}%'.format(baek[2:4]))
