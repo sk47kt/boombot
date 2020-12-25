e4edd5d7e5a520fcb2875d703d0cb102fba67e4c
sk47kt <65600966+sk47kt@users.noreply.github.com>
2020-12-25 18:11:28 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,21 @@
 import discord
-import pyupbit
-import time
+from discord.ext import commands
 import os
-client = discord.Client()
-channel = client.get_channel(791857271776477184)
-xrpprice = pyupbit.get_current_price("KRW-XRP")
-df = pyupbit.get_ohlcv("KRW-XRP", count=3)
-sung=str(round(xrpprice/304,3))
-baek=str(round(xrpprice/350,3))
-@client.event
-async def on_ready():
-    print(f'{client.user} 에 로그인하였습니다!')
+
+client = commands.Bot(command_prefix = '-')
 
 @client.event
-async def on_message(message):
-    if message.content.startswith('리플'):
-      while True:
-        await message.channel.send('현재가 : {0}'.format(xrpprice))
-        await message.channel.send('최근3일가격{0}'.format(df))
-        await message.channel.send('백종인[350] 수익률{0}%'.format(baek[2:4]))
-        await message.channel.send('정성민[304] 수익률{0}%'.format(sung[2:4]))
-        time.sleep(120)
-client.run('NzkxODUyMzkxMTAzNDYzNDY1.X-VMAQ.7OQCKpZ-c5eiwfuBGRlME642a8U')
+async def on_ready():
+
+  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
+  await client.change_presence(status=discord.Status.online)
+
+  await client.change_presence(activity=discord.Game(name="게임 하는중"))
+  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
+  
+  print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
+
+
+client.run(os.environ['token'])
