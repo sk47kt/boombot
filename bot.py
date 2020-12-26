5ea67bf6f31d9762c32776a1a426997945a01473
sk47kt <65600966+sk47kt@users.noreply.github.com>
2020-12-26 17:49:40 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,15 +17,16 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('리플'):
-      await message.channel.send('현재가 : {0}'.format(xrpprice))
-      await message.channel.send('최근3일가격{0}'.format(df))
-      if (350/xrpprice)<1:
-        await message.channel.send('백종인[350] 수익{0}%'.format(baek_p[2:4]))
-      else:
-        await message.channel.send('백종인[350] 손실{0}%'.format(baek_m[2:4]))
-      if (304/xrpprice)<1:
-        await message.channel.send('정성민[304] 수익{0}%'.format(sung_p[2:4]))
-      else:
-        await message.channel.send('정성민[304] 손실{0}%'.format(sung_m[2:4]))
-      time.sleep(1800)
+      while True:
+        await message.channel.send('현재가 : {0}'.format(xrpprice))
+        await message.channel.send('최근3일가격{0}'.format(df))
+        if (350/xrpprice)<1:
+          await message.channel.send('백종인[350] 수익{0}%'.format(baek_p[2:4]))
+        else:
+          await message.channel.send('백종인[350] 손실{0}%'.format(baek_m[2:4]))
+        if (304/xrpprice)<1:
+          await message.channel.send('정성민[304] 수익{0}%'.format(sung_p[2:4]))
+        else:
+          await message.channel.send('정성민[304] 손실{0}%'.format(sung_m[2:4]))
+        time.sleep(1800)
 client.run(os.environ['token'])
