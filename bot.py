55ff914632c3938cff37b621d623572a25faa606
sk47kt <65600966+sk47kt@users.noreply.github.com>
2020-12-26 11:22:45 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,23 @@
 import discord
 import pyupbit
 import time
-import os
+xrpprice = pyupbit.get_current_price("KRW-XRP")
+df = pyupbit.get_ohlcv("KRW-XRP", count=3)
+sung=str(round(xrpprice/304,3))
+baek=str(round(xrpprice/350,3))
 client = discord.Client()
+channel = client.get_channel(791857271776477184)
 @client.event
 async def on_ready():
     print(f'{client.user} 에 로그인하였습니다!')
+    
+
 @client.event
 async def on_message(message):
     if message.content.startswith('리플'):
-        while True:
-            xrpprice = pyupbit.get_current_price("KRW-XRP")
-            df = pyupbit.get_ohlcv("KRW-XRP", count=3)
-            sung1=str(round(xrpprice/304,3))
-            baek1=str(round(xrpprice/350,3))
-            sung=str(round(1-(304/xrpprice)))
-            baek=str(round(1-(350/xrpprice)))
-            await message.channel.send(time.strftime('%c', time.localtime(time.time())))
-            await message.channel.send('현재가격(30분갱신) : {0}'.format(xrpprice))
-            await message.channel.send('최근3일가격{0}'.format(df))
-            if (350/xrpprice)<1:
-                await message.channel.send('백종인[350] 손실율{0}%'.format(baek[2:4]))
-            elif:
-                await message.channel.send('백종인[350] 수익률{0}%'.format(baek1[2:4]))
-            if (304/xrpprice)<1:
-                await message.channel.send('정성민[304] 손실율{0}%'.format(sung[2:4]))
-            elif:
-                await message.channel.send('정성민[304] 수익률{0}%'.format(sung1[2:4]))
-            time.sleep(1800)
-client.run(os.environ['token'])
+      await message.channel.send('현재가 : {0}'.format(xrpprice))
+      await message.channel.send('최근3일가격{0}'.format(df))
+      await message.channel.send('백종인[350] 수익률{0}%'.format(baek[2:4]))
+      await message.channel.send('정성민[304] 수익률{0}%'.format(sung[2:4]))
+      time.sleep(120)
+client.run('NzkxODUyMzkxMTAzNDYzNDY1.X-VMAQ.IUGjQ5Bok5vdrPo7f0deb_8_F4M')
