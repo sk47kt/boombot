e65de91bbf3a6e59200db391c7a88e976a20246b
sk47kt <65600966+sk47kt@users.noreply.github.com>
2020-12-26 11:05:18 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,13 @@ async def on_message(message):
             await message.channel.send(time.strftime('%c', time.localtime(time.time())))
             await message.channel.send('현재가격(30분갱신) : {0}'.format(xrpprice))
             await message.channel.send('최근3일가격{0}'.format(df))
-            await message.channel.send('백종인[350] 수익률{0}%'.format(baek[2:4]))
-            await message.channel.send('정성민[304] 수익률{0}%'.format(sung[2:4]))
+            if (350/xrpprice)<1:
+                await message.channel.send('백종인[350] 손실율{0}%'.format((1-(350/xrpprice)[2:4])
+            elif:
+                await message.channel.send('백종인[350] 수익률{0}%'.format(baek[2:4]))
+            if (304/xrpprice)<1:
+                await message.channel.send('정성민[304] 손실율{0}%'.format(1-(304/xrpprice)[2:4])
+            elif:
+                await message.channel.send('정성민[304] 수익률{0}%'.format(sung[2:4]))
             time.sleep(1800)
 client.run(os.environ['token'])
