ba478c84e4e40a3d612ef59df7f4f4dec9073160
sk47kt <65600966+sk47kt@users.noreply.github.com>
2020-12-26 11:54:31 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,10 @@ import time
 import os
 xrpprice = pyupbit.get_current_price("KRW-XRP")
 df = pyupbit.get_ohlcv("KRW-XRP", count=3)
-sung=str(round(xrpprice/304,3))
-baek=str(round(xrpprice/350,3))
+sung_p=str(round(xrpprice/304,3))
+baek_p=str(round(xrpprice/350,3))
+sung_m=str(round(1-(xrpprice/304),3))
+baek_m=str(round(1-(xrpprice/354),3))
 client = discord.Client()
 channel = client.get_channel(791857271776477184)
 @client.event
@@ -19,12 +21,12 @@ async def on_message(message):
       await message.channel.send('현재가 : {0}'.format(xrpprice))
       await message.channel.send('최근3일가격{0}'.format(df))
       if (350/xrpprice)<1:
-          await message.channel.send('백종인[350] 수익률{0}%'.format(round(xrpprice/350,3)[2:4]))
+        await message.channel.send('백종인[350] 수익{0}%'.format(baek_p[2:4]))
       else:
-          await message.channel.send('백종인[350] 손실율{0}%'.format(baek[2:4]))
+        await message.channel.send('백종인[350] 손실{0}%'.format(baek_m[2:4]))
       if (304/xrpprice)<1:
-          await message.channel.send('정성민[304] 수익률{0}%'.format(round(xrpprice/304,3)[2:4]))
+        await message.channel.send('정성민[304] 수익{0}%'.format(sung_p[2:4]))
       else:
-          await message.channel.send('정성민[304] 손실율{0}%'.format(sung[2:4]))
+        await message.channel.send('정성민[304] 손실{0}%'.format(sung_m[2:4]))
       time.sleep(1800)
 client.run(os.environ['token'])
